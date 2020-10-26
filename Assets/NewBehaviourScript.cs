cdd34213bfbfe406f969b02c77dd4a9aff144057
ayase <paper.airplane01@gmail.com>
2020-10-26 13:31:24 +0900

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private Shader shader = default;
+    [SerializeField] private ComputeShader cs = default;
+    private Material mat = default;
+    private ComputeBuffer posBuf;
+    private float[] pos = new float[] { 0.0f, 0.0f, 0.0f };
+    private int kerIdx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = new Material(shader);
+        posBuf = new ComputeBuffer(1, sizeof(float) * 3);
+        posBuf.SetData(pos);
+        kerIdx = cs.FindKernel("CSMain");
+        cs.SetBuffer(kerIdx, "_Position", posBuf);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnRenderObject()
+    {
+        mat.SetBuffer("pos", posBuf);
+        if (mat.SetPass(0))
+        {
+            Graphics.DrawProceduralNow(MeshTopology.Points, 1);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        posBuf.Release();
+    }
+}
---
+++ Assets/NewSurfaceShader.shader
@@ -0,0 +1,41 @@
+﻿Shader "Custom/NewSurfaceShader"
+{
+    SubShader
+    {
+        ZWrite On
+
+        Pass 
+        {            
+            CGPROGRAM
+
+            // シェーダーモデルは5.0を指定しておきます。
+            #pragma target 5.0
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "UnityCG.cginc"
+
+            StructuredBuffer<float3> pos;
+
+            struct v2f
+            {
+                float4 pos : SV_POSITION;
+                float sz : PSIZE;
+            };
+
+            v2f vert(uint id: SV_VertexID)
+            {
+                v2f output;
+                output.pos = float4(pos[id], 1.0);
+                output.sz = 100.0;
+                return output;
+            }
+
+            fixed4 frag(v2f input) : COLOR
+            {
+                return float4(1.0, 1.0, 1.0, 1.0);
+            }
+
+            ENDCG
+        }
+    }
+}
