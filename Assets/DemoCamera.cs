4399b00e550ae8e20e7e834e0e7bb86c24a2e303
ayase <paper.airplane01@gmail.com>
2020-11-02 00:40:29 +0900

    マウスによるカメラの移動を有効にしました。

---
+++ Assets/DemoCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target = default;
+    [SerializeField] private Vector2 rotationSpeed = default;
+    private Vector2 lastMousePos = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LookAt(target);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastMousePos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            var x = lastMousePos.x - Input.mousePosition.x;
+            var y = Input.mousePosition.y - lastMousePos.y;
+            // 移動量が多い方のみを有効とします。
+            if (Mathf.Abs(x) < Mathf.Abs(y))
+            {
+                x = 0.0f;
+            } else
+            {
+                y = 0.0f;
+            }
+            var angleX = x * rotationSpeed.x;
+            var angleY = y * rotationSpeed.y;
+
+            transform.RotateAround(target.transform.position, Vector3.up, angleX);
+            transform.RotateAround(target.transform.position, transform.right, angleY);
+
+            lastMousePos = Input.mousePosition;
+        }
+    }
+}
